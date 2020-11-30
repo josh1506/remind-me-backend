b20e55d13eae5d0d94721cc1d7a71f65322646a8
josh1506 <jmjabor@gmail.com>
2020-11-30 14:53:16 +0800

    Adding User disable, recover and delete account

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from .views import UserDetailView
+from .views import (UserDetailView, UserDisableAccountView,
+                    UserRecoverAccountView, UserDeleteAccountView)
 
 
 urlpatterns = [
-    path('details/<username>/', UserDetailView.as_view(), name='update-user')
+    path('details/<username>/', UserDetailView.as_view(), name='update-user'),
+    path('disable-account/<username>/',
+         UserDisableAccountView.as_view(), name='disable-account'),
+    path('recover-account/<username>/',
+         UserRecoverAccountView.as_view(), name='recover-account'),
+    path('delete-account/<username>/',
+         UserDeleteAccountView.as_view(), name='delete-account')
 ]
--- users/views.py
+++ users/views.py
@@ -60,3 +60,51 @@ class UserDetailView(GenericAPIView):
 
         else:
             return Response({'error': 'Username is invalid.'}, status=status.HTTP_404_NOT_FOUND)
+
+
+class UserDisableAccountView(GenericAPIView):
+    def patch(self, request, username):
+        if User.objects.filter(username=username).exists():
+            user = User.objects.get(username=username)
+
+            if not user.is_verified:
+                return Response({'error': 'Email is not verified. Please check your email inbox or spam folder to verify account.'}, status=status.HTTP_400_BAD_REQUEST)
+
+            if user.is_active:
+                user.is_active = False
+                user.save()
+
+            return Response({'success': 'Your account is now disabled.'}, status=status.HTTP_200_OK)
+
+        else:
+            return Response({'error': 'Username is invalid.'}, status=status.HTTP_404_NOT_FOUND)
+
+
+class UserRecoverAccountView(GenericAPIView):
+    def patch(self, request, username):
+        if User.objects.filter(username=username).exists():
+            user = User.objects.get(username=username)
+
+            if not user.is_verified:
+                return Response({'error': 'Email is not verified. Please check your email inbox or spam folder to verify account.'}, status=status.HTTP_400_BAD_REQUEST)
+
+            if not user.is_active:
+                user.is_active = True
+                user.save()
+
+            return Response({'success': 'Your account is now activated.'}, status=status.HTTP_200_OK)
+
+        else:
+            return Response({'error': 'Username is invalid.'}, status=status.HTTP_404_NOT_FOUND)
+
+
+class UserDeleteAccountView(GenericAPIView):
+    def delete(self, request, username):
+        if User.objects.filter(username=username).exists():
+            user = User.objects.get(username=username)
+            user.delete()
+
+            return Response({'success': 'Your account is now closed, you will no longer have access to your account'}, status=status.HTTP_200_OK)
+
+        else:
+            return Response({'error': 'Username is invalid.'}, status=status.HTTP_404_NOT_FOUND)
