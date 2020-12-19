74ffa91e0ebbe251428ccccaebbc78c6cbd6fc1a
josh1506 <jmjabor@gmail.com>
2020-12-20 00:47:19 +0800

    fixing error in deployment

--- remind_me/settings.py
+++ remind_me/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = '*v@ixhvfhr$e59upg89x2r3v0*o(d_yorazck*ma4nh4rti)7='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
