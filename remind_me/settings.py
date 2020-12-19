ffff4dc073693c5df5d597519ab1c5e52a813d2a
josh1506 <jmjabor@gmail.com>
2020-12-19 17:48:56 +0800

    Adding allowed CORS

--- remind_me/settings.py
+++ remind_me/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'storages',
     'authentication',
     'social_auth',
@@ -61,7 +62,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
 
 ROOT_URLCONF = 'remind_me.urls'
 
