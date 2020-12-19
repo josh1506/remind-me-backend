8fd4f7a07db92a256f3f9dcc247d7fa5759da2f8
josh1506 <jmjabor@gmail.com>
2020-12-19 17:59:36 +0800

    Allowing all origin for test deployment

--- remind_me/settings.py
+++ remind_me/settings.py
@@ -57,15 +57,15 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
+CORS_ALLOW_ALL_ORIGINS
 
 ROOT_URLCONF = 'remind_me.urls'
 
